b69307101c9f7b2b74e5bff5663b9433f74b1c60
Isaac <ireedschwartz@ithaca.edu>
2023-04-26 13:51:11 -0400

    Notes on lyrics genius API library

--- genius_lyrics_library.py
+++ genius_lyrics_library.py
@@ -1,11 +1,22 @@
 import lyricsgenius
+from random import randint
 
-genius = lyricsgenius.Genius("-MiAj7fIqCsxQ0Unn95GJVh6DlRwnyiQjG16nDTk_MDoowDwekuJX5pwVZLRcmng") # creates an object which stores the Genius API request
-genius.remove_section_headers = True # removes things like [chorus] or [verse 1]
-artist = genius.search_artist("The Strokes", max_songs=5, sort="popularity")
-# by removing max_songs it will store all songs from artist
-# you can sort by: "title", "popularity"
-print(artist.songs)
 
-song = artist.song("Selfless")
-print(song.lyrics)
+def choose_song(genius, artist_name):
+    """chooses a song from an artists 5 most popular songs"""
+    artist_obj = genius.search_artist(artist_name, max_songs=5, sort="popularity")
+    # ^you can sort by: "title", "popularity"
+    # ^^by removing max_songs it will store all songs from artist
+    songs = artist_obj.songs
+    song_num = randint(1, 5)
+
+
+
+def main():
+    genius = lyricsgenius.Genius("-MiAj7fIqCsxQ0Unn95GJVh6DlRwnyiQjG16nDTk_MDoowDwekuJX5pwVZLRcmng")
+    # ^creates an object which stores the Genius API request
+    genius.remove_section_headers = True
+    # ^removes things like [chorus] or [verse 1]
+    songs = choose_song(genius, "Faye Webster")
+
+main()
