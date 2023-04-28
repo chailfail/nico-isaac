002559f3c9aa50481ab742b9ece5a795e61524e8
nicogoodbar <nicogoodbar@gmail.com>
2023-04-28 18:47:18 -0400

    split code into separate functions

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
+import os
 import requests
 from requests_oauthlib import OAuth2Session
 from requests.auth import HTTPBasicAuth
-import os
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
-
-# Credentials you get from registering a new application
 client_id = '312a3338244443048ebaea1d463ab3bb'
 client_secret = '6dc91bcc6e224d578eb943b890235fc4'
 redirect_uri = 'http://localhost:9000'
@@ -17,44 +15,41 @@ token_url = "https://accounts.spotify.com/api/token"
 # https://developer.spotify.com/documentation/general/guides/authorization/scopes/
 scope = ["user-top-read"]
 
-spotify = OAuth2Session(client_id, scope=scope, redirect_uri=redirect_uri)
 
-# Redirect user to Spotify for authorization
-authorization_url, state = spotify.authorization_url(authorization_base_url)
-print('Please go here and authorize: ', authorization_url)
+def authorize_and_get_token():
+    spotify = OAuth2Session(client_id, scope=scope, redirect_uri=redirect_uri)
+    # Redirect user to Spotify for authorization
+    authorization_url, state = spotify.authorization_url(authorization_base_url)
+    print('Please go here to authorize: ', authorization_url)
 
-# Get the authorization verifier code from the callback url
-redirect_response = input('\nPaste the full redirect URL here: ')
+    # Get the authorization verifier code from the callback url
+    redirect_response = input('\nPaste the full redirect URL here: ')
 
-auth = HTTPBasicAuth(client_id, client_secret)
+    auth = HTTPBasicAuth(client_id, client_secret)
 
-# Fetch the access token
-token = spotify.fetch_token(token_url, auth=auth, authorization_response=redirect_response)
-
-
-class SpotifyUserData:
-    def __init__(self, user_json):
-        top_artists_list = []
-        for item in user_json["items"]:
-            top_artists_list.append(item["name"])
-        self.top_artists_list = top_artists_list
+    # Fetch the access token
+    token = spotify.fetch_token(token_url, auth=auth, authorization_response=redirect_response)
+    return token["access_token"]
 
 
-def main():
-    access_token = token["access_token"]
-
+def user_top_artists(access_token):
     # Set the header with the access token
     headers = {
         "Authorization": f"Bearer {access_token}"
     }
-
     spotify_api_call = requests.get("https://api.spotify.com/v1/me/top/artists?limit=10&offset=0", headers=headers)
     spotify_json = spotify_api_call.json()
-    spotify_data = SpotifyUserData(spotify_json)
-    user_top_artists = spotify_data.top_artists_list
+    top_artists_list = []
+    for item in spotify_json["items"]:
+        top_artists_list.append(item["name"])
     print("\nUser's top artists:")
-    for artist in user_top_artists:
+    for artist in top_artists_list:
         print(artist)
+
+
+def main():
+    access_token = authorize_and_get_token()
+    user_top_artists(access_token)
 
 
 main()
