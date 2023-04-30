a7fd7e5b2fb5285cc7f6a4abd9692a1e5bf833fb
nicogoodbar <nicogoodbar@gmail.com>
2023-04-30 19:43:14 -0400

    set up tests

--- tests/spotify_tests.py
+++ tests/spotify_tests.py
@@ -1,10 +1,25 @@
 import unittest
-from main import *
+import requests
+from spotify_user import SpotifyUser
+from main import authorize_and_get_token
+
+access_token = authorize_and_get_token()
+headers = {
+    "Authorization": f"Bearer {access_token}"
+}
+spotify_api_call = requests.get("https://api.spotify.com/v1/me", headers=headers)
+spotify_json = spotify_api_call.json()
+current_user = SpotifyUser(spotify_json)
+test_user = SpotifyUser(spotify_json)
 
 
-class SpotifyTests(unittest.TestCase):
+class SpotifyUserTests(unittest.TestCase):
+
     def test_user_top_artists(self):
-        self.assertEqual(user_top_artists(), False)
+        self.assertEqual(test_user.get_user_top_artists(access_token), [])
+
+    def test_user_top_tracks(self):
+        self.assertEqual(test_user.get_user_top_tracks(access_token), [])
 
 
 if __name__ == '__main__':
